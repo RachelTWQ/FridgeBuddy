f393c692b34c9aab3fcafb9eeb0bfd0a58ad8153
RachelTWQ <racheltang15@hotmail.com>
2019-06-04 15:00:59 -0400

    change color schema

--- fridge-buddy/src/NavBar.jsx
+++ fridge-buddy/src/NavBar.jsx
@@ -11,7 +11,7 @@ function Logout({ handleLogout }) {
 const linkStyle = { 
   textDecoration: 'none', 
   color: 'white',  
-  background: '#5AB9EA',
+  background: '#ED5752',
   borderRadius: '5px',
   border: 'none',
   boxShadow: '1px 1px 0px 2px rgba (0,0,0,0.3)',
